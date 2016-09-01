5e540fcf9f47743120160c73b8e5de489b56744f
Jarrett Revels <jarrettrevels@gmail.com>
2016-09-01 14:22:31 -0400

    add API methods for simultaneous gradients/jacobians w.r.t. to multiple inputs

--- src/api.jl
+++ src/api.jl
@@ -1,6 +1,9 @@
-######################
-# gradient/gradient! #
-######################
+#############################################
+# Gradient of `f(::AbstractArray...)::Real` #
+#############################################
+
+# utilities #
+#-----------#
 
 load_grad_result!(out, result, xtr) = adjoint!(out, xtr)
 
@@ -10,23 +13,51 @@ function load_grad_result!(out::GradientResult, result, xtr)
     return out
 end
 
-function gradient(f, x, xtr = wrap(x))
+# gradient #
+#----------#
+
+function gradient(f, x, tr::Trace = Trace(), xtr = wrap(x, tr))
     result = f(xtr)
     seed!(result)
-    backprop!(get(trace(result)))
+    backprop!(tr)
     return adjoint(xtr)
 end
 
-function gradient!(out, f, x, xtr = wrap(eltype(out), x))
+function gradient(f, xs::Tuple, tr::Trace = Trace(),
+                  xtrs::Tuple = map(x -> wrap(x, tr), xs))
+    result = f(xtrs...)
+    seed!(result)
+    backprop!(tr)
+    return map(adjoint, xtrs)
+end
+
+# gradient! #
+#-----------#
+
+function gradient!(out, f, x, tr::Trace = Trace(), xtr = wrap(eltype(out), x, tr))
     result = f(xtr)
     seed!(result)
-    backprop!(get(trace(result)))
+    backprop!(tr)
     return load_grad_result!(out, result, xtr)
 end
 
-######################
-# jacobian/jacobian! #
-######################
+function gradient!(outs::Tuple, f, xs::Tuple, tr::Trace = Trace(),
+                   xtrs::Tuple = map((out, x) -> wrap(eltype(out), x, tr), outs, x))
+    result = f(xtrs...)
+    seed!(result)
+    backprop!(tr)
+    for i in eachindex(outs)
+        load_grad_result!(outs[i], result, xtrs[i])
+    end
+    return outs
+end
+
+######################################################
+# Jacobian of `f(::AbstractArray...)::AbstractArray` #
+######################################################
+
+# utilities #
+#-----------#
 
 function load_jacobian!(out, xtr, y, tr::Trace)
     outmatrix = reshape(out, length(y), length(xtr))
@@ -51,30 +82,54 @@ function load_jac_result!(out::JacobianResult, xtr, ytr, tr)
     return out
 end
 
-function jacobian(f, x, xtr = wrap(x))
-    tr = get(trace(first(xtr)))
+# jacobian #
+#----------#
+
+function jacobian(f, x, tr::Trace = Trace(), xtr = wrap(x, tr))
     ytr = f(xtr)
     out = similar(ytr, eltype(x), length(ytr), length(x))
     return load_jacobian!(out, xtr, ytr, tr)
 end
 
-function jacobian!(out, f, x, xtr = wrap(eltype(out), x))
+function jacobian(f, xs::Tuple, tr::Trace = Trace(),
+                  xtrs::Tuple = map(x -> wrap(x, tr), xs))
+    ytr = f(xtrs...)
+    outs = map(x -> similar(ytr, eltype(x), length(ytr), length(x)), xs)
+    for i in eachindex(outs)
+        load_jacobian!(outs[i], xtrs[i], ytr, tr)
+    end
+    return outs
+end
+
+# jacobian! #
+#-----------#
+
+function jacobian!(out, f, x, tr::Trace = Trace(), xtr = wrap(eltype(out), x, tr))
     tr = get(trace(first(xtr)))
     ytr = f(xtr)
     return load_jac_result!(out, xtr, ytr, tr)
+end
+
+function jacobian!(outs::Tuple, f, xs::Tuple, tr::Trace = Trace(),
+                   xtrs::Tuple = map((out, x) -> wrap(eltype(out), x, tr), outs, x))
+   ytr = f(xtrs...)
+   for i in eachindex(outs)
+       load_jac_result!(outs[i], xtrs[i], ytr, tr)
+   end
+   return outs
 end
 
 ####################
 # hessian/hessian! #
 ####################
 
-hessian(f, x, xtr = wrap(x)) = jacobian(y -> gradient(f, y), x, xtr)
-
-hessian!(out, f, x, xtr = wrap(x)) = jacobian!(out, y -> gradient(f, y), x, xtr)
-
-function hessian!(out::HessianResult, f, x, xtr = wrap(x))
-    outgrad = GradientResult(out.value, out.gradient)
-    jacobian!(out.hessian, y -> gradient!(outgrad, y), x, xtr)
-    out.value = outgrad.value
-    return out
-end
+# hessian(f, x, xtr = wrap(x)) = jacobian(y -> gradient(f, y), x, xtr)
+#
+# hessian!(out, f, x, xtr = wrap(x)) = jacobian!(out, y -> gradient(f, y), x, xtr)
+#
+# function hessian!(out::HessianResult, f, x, xtr = wrap(x))
+#     outgrad = GradientResult(out.value, out.gradient)
+#     jacobian!(out.hessian, y -> gradient!(outgrad, y), x, xtr)
+#     out.value = outgrad.value
+#     return out
+# end
