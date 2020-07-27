4631cfefd63c5d67c8c2ff2ccf5bea752e5e6a8c
Mohamed Tarek <mohamed82008@gmail.com>
2020-07-28 00:12:01 +1000

    remove tracker workaround for Julia 1.0 (#138)

--- src/derivatives/broadcast.jl
+++ src/derivatives/broadcast.jl
@@ -101,35 +101,6 @@ function Base.copy(_bc::Broadcasted{TrackedStyle})
     end
 end
 
-# https://github.com/FluxML/Flux.jl/issues/353
-if VERSION < v"1.1.0-DEV.548"
-    @eval Base.Broadcast begin
-        function flatten(bc::Broadcasted{Style}) where {Style}
-            isflat(bc) && return bc
-            args = cat_nested(bc)
-            let makeargs = make_makeargs(bc), f = bc.f
-                newf = @inline function(args::Vararg{Any,N}) where N
-                f(makeargs(args...)...)
-                end
-                return Broadcasted{Style}(newf, args, bc.axes)
-            end
-        end
-        @inline function make_makeargs(makeargs, t::Tuple{<:Broadcasted,Vararg{Any}})
-            bc = t[1]
-            let makeargs = make_makeargs(makeargs, tail(t)), f = bc.f
-                let makeargs = make_makeargs(makeargs, bc.args)
-                    headargs, tailargs = make_headargs(bc.args), make_tailargs(bc.args)
-                    return @inline function(args::Vararg{Any,N}) where N
-                        args1 = makeargs(args...)
-                        a, b = headargs(args1...), tailargs(args1...)
-                        (f(a...), b...)
-                    end
-                end
-            end
-        end
-    end
-end
-
 getouttype(::TrackedReal{<:Any, D}) where {D} = D
 getouttype(::TrackedArray{<:Any, D}) where {D} = D
 getouttype(::Any) = Union{}
