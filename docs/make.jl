d1881800e53009b997a6f6ed430b1e77b8630ff4
hyrodium <hyrodium@gmail.com>
2021-11-28 23:16:02 +0900

    update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,20 @@
 using Documenter, ReverseDiff
 
-makedocs(modules=[ReverseDiff], sitename="http://www.juliadiff.org/ReverseDiff.jl")
+makedocs(;
+    modules=[ReverseDiff],
+    sitename="ReverseDiff.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="http://www.juliadiff.org/ReverseDiff.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Limitation of ReverseDiff" => "limits.md",
+        "API" => "api.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/JuliaDiff/ForwardDiff.jl",
+)
