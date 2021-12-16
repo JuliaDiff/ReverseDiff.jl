108a1838419a95f5ae7d64e4a08efcb0bd01f9e9
David Widmann <dev+git@devmotion.de>
2021-12-17 00:02:36 +0100

    Add preview

--- docs/make.jl
+++ docs/make.jl
@@ -4,17 +4,18 @@ makedocs(;
     modules=[ReverseDiff],
     sitename="ReverseDiff.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
+        prettyurls=get(ENV, "CI", nothing) == "true",
         canonical="http://www.juliadiff.org/ReverseDiff.jl",
-        assets=String[],
     ),
     pages=[
         "Home" => "index.md",
         "Limitation of ReverseDiff" => "limits.md",
         "API" => "api.md",
     ],
+    strict=true,
+    checkdocs=:exports,
 )
 
 deploydocs(;
-    repo="github.com/JuliaDiff/ForwardDiff.jl",
+    repo="github.com/JuliaDiff/ForwardDiff.jl", push_preview=true,
 )
