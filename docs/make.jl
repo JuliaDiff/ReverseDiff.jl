c3c96ebf07b2340a1e8e181d46eb2c796e527a9a
David Widmann <devmotion@users.noreply.github.com>
2021-12-21 01:42:45 +0100

    Fix documentation (https://github.com/JuliaDiff/ReverseDiff.jl/pull/190 with preview) (#193)
    * remove mkdocs.yml
    
    * update docs/make.jl
    
    * Add preview
    
    * Update CI
    
    * Fix repo url
    
    Co-authored-by: hyrodium <hyrodium@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,21 @@
 using Documenter, ReverseDiff
 
-makedocs(modules=[ReverseDiff], sitename="http://www.juliadiff.org/ReverseDiff.jl")
+makedocs(;
+    modules=[ReverseDiff],
+    sitename="ReverseDiff.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", nothing) == "true",
+        canonical="http://www.juliadiff.org/ReverseDiff.jl",
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Limitation of ReverseDiff" => "limits.md",
+        "API" => "api.md",
+    ],
+    strict=true,
+    checkdocs=:exports,
+)
+
+deploydocs(;
+    repo="github.com/JuliaDiff/ReverseDiff.jl", push_preview=true,
+)
