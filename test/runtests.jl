21f6e416a5069d968622cfb2a7d0f122e8b34a2a
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-07-29 12:31:44 +0200

    No testsets on 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ const TESTDIR = dirname(@__FILE__)
 
 test_println(kind, f, pad = "  ") = println(pad, "testing $(kind): `$(f)`...")
 
-@testset verbose = true "ReverseDiff" begin
+@testset "ReverseDiff" begin
 
 println("running TapeTests...")
 t = @elapsed include(joinpath(TESTDIR, "TapeTests.jl"))
